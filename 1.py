654554f658dd97b3b28504e61e3d139c9c2ce1e3
Car Jeff <49448671+carjeff@users.noreply.github.com>
2019-11-06 22:21:37 +0800

    add dehaze code and seamCarving code“

--- 1.py
+++ 1.py
@@ -1,23 +1,41 @@
-import numpy as np
-import glob
-
-from PIL import Image
-
-f = glob.glob('./image/lena.jpg')
-
-PicData = np.zeros((1000, 98306))
-
-for i in range(1000):
-    img = Image.open(f[i]).convert('L')
-    img = Image.open(f[i])
-    img = img.resize((256,256))
-    img = np.array(img)
-    m, n = img.shape
-    im = img.reshape((-1, 1))
-    PicData[i, :98304] = im[:, 0]
-    PicData[i, 98304] = m
-    PicData[i, 98305] = n
-
-print(PicData.shape)
-# print(PicData[:,1])
-np.save('PicData', PicData)
+import cv2
+im=cv2.imread('./image/lena.jpg')
+cv2.imshow('init',im)
+cv2.waitKey(5)
+row,col,channel=im.shape
+increment=50
+increment=increment/100
+for i in range(row):
+    for j in range(col):
+        rgbmax=max(im[i][j][0],im[i][j][1],im[i][j][2])
+        rgbmin=min(im[i][j][0],im[i][j][1],im[i][j][2])
+        delta=(int(rgbmax)-int(rgbmin))/255
+        if delta==0:
+            continue
+        value=(int(rgbmax)+int(rgbmin))/255*1.0
+        l=value/2
+        if l<0.5:
+            s=delta/value
+        else:
+            if 2-value==0:
+                s=10
+            else:
+                s=delta/(2-value)
+        if increment>0:
+            if increment+s>=1:
+                alpha=s
+            else:
+                alpha=1-increment
+            alpha=1/alpha-1;
+            im[i][j][0]=im[i][j][0]+(im[i][j][0]-l*255)*alpha
+            im[i][j][1] = im[i][j][1] + (im[i][j][1] - l * 255) * alpha
+            im[i][j][2] = im[i][j][2] + (im[i][j][2] - l * 255) * alpha
+        else:
+            alpha=increment
+            im[i][j][0]=l*255+(im[i][j][0]-l*255)*(1+alpha)
+            im[i][j][1] = l * 255 + (im[i][j][1] - l * 255) * (1 + alpha)
+            im[i][j][2] = l * 255 + (im[i][j][2] - l * 255) * (1 + alpha)
+cv2.imshow('result',im)
+# cv2.namedWindow('result')
+# cv2.createTrackbar('size','result',scale,4,contrast)
+cv2.waitKey(0)
